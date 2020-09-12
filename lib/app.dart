9c14a3df8cf3035c4d2f8c723c60b1ab82791089
WISNU GINANJAR SAPUTRA <wisnuwiry@gmail.com>
2020-09-12 13:24:07 +0700

    fix localization

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'flavors.dart';
+import 'generated/l10n.dart';
 import 'modules/auth/auth.dart';
 
 class App extends StatelessWidget {
@@ -7,6 +10,13 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: F.title,
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),

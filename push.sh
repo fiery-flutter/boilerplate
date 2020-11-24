6f089f44584233877631815786c636b0a3de8cf6
WISNU GINANJAR SAPUTRA <wisnuwiry@gmail.com>
2020-11-25 06:35:26 +0700

    👌 IMPROVE: automation

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 flutter format --set-exit-if-changed lib/component lib/core lib/features lib/main_dev.dart lib/main_stagging.dart lib/main_prod.dart test
+flutter test && flutter test --coverage
 
+git add .
+echo "Input commit message, after formatting code"
+read commit
+git commit -m "${commit}"
 git push
